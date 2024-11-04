f0d416b325ca96ddfbfaf49fca5af7168a7184e2
Frank Elsinga <frank.elsinga@tum.de>
2024-11-04 16:44:37 +0100

    remove deprecated + unimplemnted HOV costing

--- src/route/costing/mod.rs
+++ src/route/costing/mod.rs
@@ -49,11 +49,6 @@ pub enum Costing {
     /// - weight limits
     #[serde(rename = "truck")]
     Truck(truck::TruckCostingOptions),
-    /// DEPRECATED: use [`Costing::Auto`] with HOV costing options.
-    #[serde(rename = "hov")]
-    // #[deprecated] <- this would cause a compilation warning due to https://github.com/rust-lang/rust/issues/92313
-    #[doc(hidden)]
-    Hov(hov::HovCostingOptions),
     /// Standard costing for taxi routes.
     ///
     /// Taxi costing inherits the [`Costing::Auto`] behaviors, but checks and favors
