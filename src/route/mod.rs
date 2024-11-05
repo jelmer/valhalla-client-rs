05b80702c50dea1e6f6ec6e4738dbf1e45a9c102
Frank Elsinga <frank.elsinga@tum.de>
2024-11-05 18:10:08 +0100

    migrate the Manifst to be a builder

--- src/route/mod.rs
+++ src/route/mod.rs
@@ -379,60 +379,18 @@ pub enum Units {
 #[derive(Serialize, Default, Debug)]
 pub struct Manifest {
     #[serde(flatten)]
-    pub costing: costing::Costing,
-
-    pub locations: Vec<Location>,
-
-    /// Distance units for output. Allowable unit types are miles (or mi) and kilometers (or km).
-    /// If no unit type is specified, the units default to kilometers.
-    pub units: Units,
-
-    /// Name your route request. If id is specified, the naming will be sent thru to the response.
-    pub id: String,
-
-    /// The language of the narration instructions based on the IETF BCP 47 language tag string. If
-    /// no language is specified or the specified language is unsupported, United States-based
-    /// English (en-US) is used. Currently supported language list
-    pub language: String,
-
-    pub directions_type: DirectionsType,
-
-    /// A number denoting how many alternate routes should be provided. There may be no alternates
-    /// or less alternates than the user specifies. Alternates are not yet supported on multipoint
-    /// routes (that is, routes with more than 2 locations). They are also not supported on time
-    /// dependent routes.
-    pub alternates: i32,
-
-    /// A set of locations to exclude or avoid within a route can be specified using a JSON array
-    /// of avoid_locations. The avoid_locations have the same format as the locations list. At a
-    /// minimum each avoid location must include latitude and longitude. The avoid_locations are
-    /// mapped to the closest road or roads and these roads are excluded from the route path
-    /// computation.
-    pub exclude_locations: Vec<Location>,
-
-    /// One or multiple exterior rings of polygons in the form of nested JSON arrays.
-    ///
-    /// Example: `[[[lon1, lat1], [lon2,lat2]],[[lon1,lat1],[lon2,lat2]]]`
-    /// Roads intersecting these rings will be
-    /// avoided during path finding. If you only need to avoid a few specific roads, it's much more
-    /// efficient to use exclude_locations. Valhalla will close open rings (i.e. copy the first
-    /// coordinate to the last position).
-    pub exclude_polygons: Vec<Vec<(f64, f64)>>,
-
-    /// When present and true, the successful route response will include a key linear_references.
-    /// Its value is an array of base64-encoded OpenLR location references, one for each graph edge
-    /// of the road network matched by the input trace.
-    pub linear_references: bool,
-
-    /// Prioritize bidirectional a* when date_time.type = depart_at/current. By default
-    /// time_dependent_forward a* is used in these cases, but bidirectional a* is much faster.
-    /// Currently, it does not update the time (and speeds) when searching for the route path, but
-    /// the ETA on that route is recalculated based on the time-dependent speeds
-    pub prioritize_bidirectional: bool,
-
-    /// A boolean indicating whether exit instructions at roundabouts should be added to the output
-    /// or not. Default is true.
-    pub roundabout_exits: bool,
+    costing: costing::Costing,
+    locations: Vec<Location>,
+    units: Units,
+    id: String,
+    language: String,
+    directions_type: DirectionsType,
+    alternates: i32,
+    exclude_locations: Vec<Location>,
+    exclude_polygons: Vec<Vec<(f32, f32)>>,
+    linear_references: Option<bool>,
+    prioritize_bidirectional: Option<bool>,
+    roundabout_exits: Option<bool>,
 }
 
 impl Manifest {
@@ -443,9 +401,206 @@ impl Manifest {
     ///
     /// Valhalla's routing service uses dynamic, run-time costing to generate the route path.
     /// Can be configured with different settings depending on the costing model used.
+    ///
     /// Default: [`costing::Costing::Auto`]
     pub fn costing(mut self, costing: costing::Costing) -> Self {
         self.costing = costing;
+        self
+    }
+
+    /// You specify locations as an ordered list of two or more locations.
+    ///
+    /// Locations are visited in the order specified.
+    /// A location must include a latitude and longitude in decimal degrees.
+    /// The coordinates can come from many input sources, such as a GPS location, a point or a
+    /// click on a map, a geocoding service, and so on.
+    ///
+    /// **Note:** Valhalla cannot search for names or addresses or perform geocoding or reverse geocoding.
+    /// External search services, such as [Mapbox Geocoding](https://www.mapbox.com/api-documentation/#geocoding),
+    /// can be used to find places and geocode addresses, which must be converted to coordinates for input.
+    ///
+    /// To build a route, you need to specify two [`LocationType::Break`] locations.
+    /// In addition, you can include [`LocationType::Through`], [`LocationType::Via`] or
+    /// [`LocationType::BreakThrough`] locations to influence the route path.
+    pub fn locations(mut self, locations: impl IntoIterator<Item = Location>) -> Self {
+        self.locations = locations.into_iter().collect();
+        debug_assert!(self.locations.len() >= 2);
+        self
+    }
+
+    /// Sets the distance units for output.
+    ///
+    /// Possible unit types are miles via [`Units::Imperial`] and kilometers via [`Units::Metric`].
+    ///
+    /// Default: [`Units::Metric`]
+    pub fn units(mut self, units: Units) -> Self {
+        self.units = units;
+        self
+    }
+
+    /// Name your route request.
+    ///
+    /// If id is specified, the naming will be sent through to the response.
+    pub fn id(mut self, id: impl ToString) -> Self {
+        self.id = id.to_string();
+        self
+    }
+
+    /// The language of the narration instructions based on the
+    /// [IETF BCP 47](https://en.wikipedia.org/wiki/IETF_language_tag) language tag string.
+    ///
+    /// If unsupported, the language `en-US` (United States-based English) is used
+    /// Currently supported language list can be found here:
+    /// <https://valhalla.github.io/valhalla/api/turn-by-turn/api-reference/#supported-language-tags>
+    ///
+    /// Default: `en-US` (United States-based English)
+    pub fn language(mut self, language: impl ToString) -> Self {
+        self.language = language.to_string();
+        self
+    }
+    /// Sets the directions type
+    ///
+    /// [`DirectionsType`] is an enum with 3 values:
+    /// - [`DirectionsType::None`] indicates no maneuvers or instructions should be returned.
+    /// - [`DirectionsType::Maneuvers`] indicates that only maneuvers be returned.
+    /// - [`DirectionsType::Instructions`] indicates that maneuvers with instructions should be returned
+    ///
+    /// Default: [`DirectionsType::Instructions`]
+    pub fn directions_type(mut self, directions_type: DirectionsType) -> Self {
+        self.directions_type = directions_type;
+        self
+    }
+
+    /// A number denoting how many alternate routes should be provided.
+    ///
+    /// There may be no alternates or fewer alternates than the user specifies.
+    ///
+    /// Alternates are not yet supported on
+    /// - multipoint routes (i.e. routes with more than 2 locations) and
+    /// - time dependent routes
+    pub fn alternates(mut self, alternates: i32) -> Self {
+        self.alternates = alternates;
+        self
+    }
+
+    /// A set of locations to exclude or avoid within a route can be specified using a JSON array
+    /// of avoid_locations.
+    ///
+    /// The avoid_locations have the same format as the locations list.
+    /// At a minimum each avoid location must include latitude and longitude.
+    /// The avoid_locations are mapped to the closest road or roads and these roads are excluded
+    /// from the route path computation.
+    pub fn exclude_locations(
+        mut self,
+        exclude_locations: impl IntoIterator<Item = Location>,
+    ) -> Self {
+        self.exclude_locations = exclude_locations.into_iter().collect();
+        self
+    }
+
+    /// Sets at least one exterior rings of excluded polygons.
+    ///
+    /// **Note:** Contrary to [`Self::exclude_polygon`], this OVERRIDES previously set excluded polygons.
+    ///
+    /// Roads intersecting these rings will be avoided during path finding.
+    /// If you only need to avoid a few specific roads, it's much more efficient to use
+    /// exclude_locations.
+    /// Valhalla will close open rings (i.e. copy the first coordinate to the last position).
+    ///
+    /// # Example:
+    /// ```rust,no_run
+    /// use valhalla_client::Valhalla;
+    /// use valhalla_client::route::{Location, Manifest};
+    /// use valhalla_client::route::costing::{Costing};
+    ///
+    /// let polygon_around_midrecht_between_amsterdam_and_utrecht = vec![(4.9904022, 52.2528761), (4.8431168, 52.2392163), (4.8468933, 52.1799052), (4.9845657, 52.2102016), (4.9904022, 52.2528761)];
+    /// let polygon_around_leiden = vec![(4.5891266, 52.1979985),(4.4105987, 52.2560249),(4.3034820, 52.1592721),(4.5005493, 52.0935286),(4.5726471, 52.1373684),(4.5898132, 52.1984193),(4.5891266, 52.1979985)];
+    /// let amsterdam = Location::new(4.9041, 52.3676);
+    /// let utrecht = Location::new(5.1214, 52.0907);
+    ///
+    /// let manifest = Manifest::builder()
+    ///   .locations([amsterdam, utrecht])
+    ///   .exclude_polygons([polygon_around_leiden, polygon_around_midrecht_between_amsterdam_and_utrecht])
+    ///   .costing(Costing::Bicycle(Default::default()));
+    ///
+    /// let response = Valhalla::default()
+    ///   .route(manifest)
+    ///   .unwrap();
+    /// # assert!(!response.legs.is_empty());
+    /// ```
+    pub fn exclude_polygons(
+        mut self,
+        exclude_polygons: impl IntoIterator<Item = impl IntoIterator<Item = (f32, f32)>>,
+    ) -> Self {
+        self.exclude_polygons = exclude_polygons
+            .into_iter()
+            .map(|e| e.into_iter().collect())
+            .collect();
+        self
+    }
+    /// Add one exterior rings as an excluded polygon.
+    ///
+    /// **Note:** Contrary to [`Self::exclude_polygons`], this APPENDS to the previously set excluded polygons.
+    ///
+    /// Roads intersecting these rings will be avoided during path finding.
+    /// If you only need to avoid a few specific roads, it's much more efficient to use
+    /// exclude_locations.
+    /// Valhalla will close open rings (i.e. copy the first coordinate to the last position).
+    ///
+    /// # Example:
+    /// ```rust,no_run
+    /// use valhalla_client::Valhalla;
+    /// use valhalla_client::route::{Location, Manifest};
+    /// use valhalla_client::route::costing::{Costing};
+    ///
+    /// let polygon_around_leiden = vec![(4.5891266, 52.1979985),(4.4105987, 52.2560249),(4.3034820, 52.1592721),(4.5005493, 52.0935286),(4.5726471, 52.1373684),(4.5898132, 52.1984193),(4.5891266, 52.1979985)];
+    /// let amsterdam = Location::new(4.9041, 52.3676);
+    /// let utrecht = Location::new(5.1214, 52.0907);
+    ///
+    /// let manifest = Manifest::builder()
+    ///   .locations([amsterdam, utrecht])
+    ///   .exclude_polygon(polygon_around_leiden)
+    ///   .costing(Costing::Bicycle(Default::default()));
+    ///
+    /// let response = Valhalla::default()
+    ///   .route(manifest)
+    ///   .unwrap();
+    /// # assert!(!response.legs.is_empty());
+    /// ```
+    pub fn exclude_polygon(
+        mut self,
+        exclude_polygon: impl IntoIterator<Item = (f32, f32)>,
+    ) -> Self {
+        self.exclude_polygons
+            .push(exclude_polygon.into_iter().collect());
+        self
+    }
+
+    /// When present and true, the successful route response will include a key linear_references.
+    ///
+    /// Its value is an array of base64-encoded [OpenLR location references](https://en.wikipedia.org/wiki/OpenLR),
+    /// one for each graph edge of the road network matched by the input trace.
+    pub fn include_linear_references(mut self) -> Self {
+        self.linear_references = Some(true);
+        self
+    }
+
+    /// Prioritize bidirectional A* when `date_time.type = depart_at/current`.
+    ///
+    /// Currently, it does not update the time (and speeds) when searching for the route path, but
+    /// the ETA on that route is recalculated based on the time-dependent speeds
+    ///
+    /// Default: time_dependent_forward A* is used in these cases, but bidirectional A* is much faster
+    pub fn prioritize_bidirectional(mut self) -> Self {
+        self.prioritize_bidirectional = Some(true);
+        self
+    }
+
+    /// Don't include instructions at roundabouts to the output
+    ///
+    /// Default: `true`
+    pub fn roundabout_exits(mut self) -> Self {
+        self.roundabout_exits = Some(false);
         self
     }
 }
@@ -633,56 +788,56 @@ mod test {
         assert_eq!(
             serde_json::to_value(manifest).unwrap(),
             serde_json::json!({
-              "costing": "auto",
-              "costing_options": {
-                  "maneuver_penalty": null,
-                  "gate_cost": null,
-                  "gate_penalty": null,
-                  "private_access_penalty": null,
-                  "destination_only_penalty": null,
-                  "toll_booth_cost": null,
-                  "toll_booth_penalty": null,
-                  "ferry_cost": null,
-                  "use_ferry": null,
-                  "use_highways": null,
-                  "use_tolls": null,
-                  "use_living_streets": null,
-                  "use_tracks": null,
-                  "service_penalty": null,
-                  "service_factor": null,
-                  "country_crossing_cost": null,
-                  "country_crossing_penalty": null,
-                  "shortest": null,
-                  "use_distance": null,
-                  "disable_hierarchy_pruning": null,
-                  "top_speed": null,
-                  "fixed_speed": null,
-                  "closure_factor": null,
-                  "ignore_closures": null,
-                  "ignore_restrictions": null,
-                  "ignore_oneways": null,
-                  "ignore_non_vehicular_restrictions": null,
-                  "ignore_access": null,
-                  "speed_types": null,
-                  "height": null,
-                  "width": null,
-                  "exclude_unpaved": null,
-                  "exclude_cash_only_tolls": null,
-                  "include_hov2": null,
-                  "include_hov3": null,
-                  "include_hot": null
-                },
-              "locations": [],
-              "units": "kilometers",
-              "id": "",
-              "language": "",
-              "directions_type": "instructions",
-              "alternates": 0,
-              "exclude_locations": [],
-              "exclude_polygons": [],
-              "linear_references": false,
-              "prioritize_bidirectional": false,
-              "roundabout_exits": false
+                  "costing": "auto",
+                  "costing_options": {
+                      "maneuver_penalty": null,
+                      "gate_cost": null,
+                      "gate_penalty": null,
+                      "private_access_penalty": null,
+                      "destination_only_penalty": null,
+                      "toll_booth_cost": null,
+                      "toll_booth_penalty": null,
+                      "ferry_cost": null,
+                      "use_ferry": null,
+                      "use_highways": null,
+                      "use_tolls": null,
+                      "use_living_streets": null,
+                      "use_tracks": null,
+                      "service_penalty": null,
+                      "service_factor": null,
+                      "country_crossing_cost": null,
+                      "country_crossing_penalty": null,
+                      "shortest": null,
+                      "use_distance": null,
+                      "disable_hierarchy_pruning": null,
+                      "top_speed": null,
+                      "fixed_speed": null,
+                      "closure_factor": null,
+                      "ignore_closures": null,
+                      "ignore_restrictions": null,
+                      "ignore_oneways": null,
+                      "ignore_non_vehicular_restrictions": null,
+                      "ignore_access": null,
+                      "speed_types": null,
+                      "height": null,
+                      "width": null,
+                      "exclude_unpaved": null,
+                      "exclude_cash_only_tolls": null,
+                      "include_hov2": null,
+                      "include_hov3": null,
+                      "include_hot": null
+                    },
+               "locations": [],
+               "units": "kilometers",
+               "id": "",
+               "language": "",
+               "directions_type": "instructions",
+               "alternates": 0,
+               "exclude_locations": [],
+               "exclude_polygons": [],
+               "linear_references": null,
+               "prioritize_bidirectional": null,
+               "roundabout_exits": null
             })
         )
     }
